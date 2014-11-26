b71d1bd2a7f22fe9dfcd58a26731a063aa41c485
Y500 <Y500@141.224.242.5>
2014-11-26 03:06:13 -0600

    coded in original url variables and methods

--- src/main/java/com/db/Db.java
+++ src/main/java/com/db/Db.java
@@ -1,6 +1,7 @@
 package com.db;
 
 //import java.net.URI;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
 
@@ -10,16 +11,16 @@ import java.sql.*;
 public class Db {
 	public static Connection getConnection() throws URISyntaxException, SQLException {
 		//?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory
-		final String URL = "postgresql://yygnxivbnbtjqp:hhipD09VgH29_AwFgJiKV-PcI_@ec2-54-243-245-159.compute-1.amazonaws.com:5432/d3f68tgt8qdh4u";   
-		final String USERNAME = "yygnxivbnbtjqp";
-		final String PASSWORD = "hhipD09VgH29_AwFgJiKV-PcI_"; 
-	    //URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		//final String URL = "postgresql://yygnxivbnbtjqp:hhipD09VgH29_AwFgJiKV-PcI_@ec2-54-243-245-159.compute-1.amazonaws.com:5432/d3f68tgt8qdh4u";   
+		//final String USERNAME = "yygnxivbnbtjqp";
+		//final String PASSWORD = "hhipD09VgH29_AwFgJiKV-PcI_"; 
+	    URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-	    //String username = dbUri.getUserInfo().split(":")[0];
-	    //String password = dbUri.getUserInfo().split(":")[1];
-	    //String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
 
-	    return DriverManager.getConnection(URL);
+	    return DriverManager.getConnection(dbUrl, username, password);
 		
 
 		

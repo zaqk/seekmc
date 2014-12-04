4f455416ad8c2a446aa5ebd080aa12268215cab8
Y500 <Y500@141.224.242.5>
2014-12-03 19:05:55 -0600

    s

--- src/main/java/com/bean/ResultSetBean.java
+++ src/main/java/com/bean/ResultSetBean.java
@@ -96,8 +96,8 @@ public class ResultSetBean {
 			linkList.add(initialLinkNameList.get(i));
 		}
 	}
-	public String[] debugList(){
-		return createSongNameListArray();
+	public String debugList(){
+		return createSongNameListArray().toString();
 	}
 
 }
